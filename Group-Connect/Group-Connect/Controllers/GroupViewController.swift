ff6867ce4af50f46e8f37d394fb9d364e0fcc1d8
Howard Wang <howardwang00@users.noreply.github.com>
2017-07-18 20:43:32 -0700

    Added Group UI and Dismissed Group Code Keyboard

--- Group-Connect/Group-Connect/Controllers/GroupViewController.swift
+++ Group-Connect/Group-Connect/Controllers/GroupViewController.swift
@@ -8,14 +8,23 @@
 
 import UIKit
 
-class GroupViewController: UIViewController {
+class GroupViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var hiUsernameLabel: UILabel!
+    @IBOutlet weak var joinButton: UIButton!
+    @IBOutlet weak var groupCodeTextField: UITextField!
+    @IBOutlet weak var createButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         hiUsernameLabel.text = "Hi \(User.current.username)!"
+        joinButton.layer.cornerRadius = 5
+        createButton.layer.cornerRadius = 5
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tap)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +32,22 @@ class GroupViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func joinButtonTapped(_ sender: Any) {
+        print(groupCodeTextField.text ?? "Group code is empty")
+    }
 
+    @IBAction func createButtonTapped(_ sender: Any) {
+        
+    }
+    
+    func dismissKeyboard() {
+        groupCodeTextField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        dismissKeyboard()
+        return true
+    }
+    
 }
 
